be496c7ee4404005cf546eb893955af1b0f2e528
梅世祺 <shiqi.mei@outlook.com>
2023-08-29 21:26:52 +0800

    feat: basic http server

---
+++ crates/README.md
@@ -0,0 +1,9 @@
+# CodeIndex
+
+CodeIndex is a local-first high performance codebase index engine designed for AI. It helps your LLM understanding the structure and semantics of a codebase and grabs code context based on the inputs.
+
+## Development
+
+```bash
+cargo watch -x run
+```
--- crates/codeindex/src/main.rs
+++ crates/codeindex/src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use serde_json::Value as JsonValue;
+
+#[derive(serde::Serialize)]
+struct CodeIndexResponse {
+    errcode: u16,
+    data: JsonValue,
+}
+
+enum CodeIndexError {
+    SomethingWentWrong,
+}
+
+impl IntoResponse for CodeIndexError {
+    fn into_response(self) -> Response {
+        let body = match self {
+            CodeIndexError::SomethingWentWrong => "Something went wrong",
+        };
+
+        (StatusCode::INTERNAL_SERVER_ERROR, body).into_response()
+    }
+}
+
+async fn handler() -> Result<(), CodeIndexError> {
+    Err(CodeIndexError::SomethingWentWrong)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/:file", get(handler));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> Html<&'static str> {
+    Html("<h1>CodeIndex API</h1>")
 }
