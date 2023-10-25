ecba463f89682a6be760a0743d4bb29ace34e8d5
梅世祺 <shiqi.mei@outlook.com>
2023-10-25 11:57:51 +0800

    feat: add javascript/typescript/python language support

--- crates/codeindex/src/main.rs
+++ crates/codeindex/src/main.rs
@@ -4,7 +4,10 @@ use std::fs;
 use ignore::overrides::OverrideBuilder;
 use ignore::WalkBuilder;
 use tree_sitter::{Node, Parser};
-use tree_sitter_rust as ts_rust;
+use {
+    tree_sitter_javascript as ts_js, tree_sitter_python as ts_python, tree_sitter_rust as ts_rust,
+    tree_sitter_typescript as ts_ts,
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut overrides = OverrideBuilder::new(".");
@@ -16,35 +19,61 @@ fn main() -> Result<(), Box<dyn Error>> {
         .build()
     {
         match result {
-            Ok(entry) => {
-                // Skip the root directory
-                if entry.depth() == 0 {
-                    continue;
-                }
-
-                // Strip the './' prefix and print the path
-                let path = entry.path().strip_prefix("./").unwrap_or(entry.path());
-                println!("{}", path.display());
-
-                // Only process .rs files
-                if path.extension() == Some(std::ffi::OsStr::new("rs")) {
-                    let code = fs::read_to_string(path)?;
-                    let mut parser = Parser::new();
-                    let language = ts_rust::language();
-                    parser.set_language(language).unwrap();
-                    let tree = parser.parse(&code, None).unwrap();
-
-                    let root_node = tree.root_node();
-                    let outline = get_outline(root_node, &code);
-                    for signature in outline {
-                        println!("  {}", signature);
-                    }
-                }
-            }
+            Ok(entry) => process_entry(entry)?,
             Err(err) => eprintln!("Error: {}", err),
         }
     }
     Ok(())
+}
+
+fn process_entry(entry: ignore::DirEntry) -> Result<(), Box<dyn Error>> {
+    // Skip the root directory
+    if entry.depth() == 0 {
+        return Ok(());
+    }
+
+    // Strip the './' prefix and print the path
+    let path = entry.path().strip_prefix("./").unwrap_or(entry.path());
+    println!("{}", path.display());
+
+    // Check if path is a file
+    if path.is_file() {
+        match fs::read_to_string(path) {
+            Ok(code) => {
+                let mut parser = Parser::new();
+                let language = get_language(path.extension().and_then(std::ffi::OsStr::to_str));
+
+                if let Some(language) = language {
+                    parser.set_language(language).unwrap();
+                } else {
+                    return Ok(()); // Ignore other file types
+                }
+
+                let tree = parser.parse(&code, None).unwrap();
+                let root_node = tree.root_node();
+                let outline = get_outline(root_node, &code);
+                for signature in outline {
+                    println!("  {}", signature);
+                }
+            }
+            Err(e) if e.kind() == std::io::ErrorKind::InvalidData => {
+                // Skip binary files
+            }
+            Err(e) => return Err(e.into()),
+        }
+    }
+    Ok(())
+}
+
+fn get_language(extension: Option<&str>) -> Option<tree_sitter::Language> {
+    match extension {
+        Some("rs") => Some(ts_rust::language()),
+        Some("js") | Some("jsx") => Some(ts_js::language()),
+        Some("ts") => Some(ts_ts::language_typescript()),
+        Some("tsx") => Some(ts_ts::language_tsx()),
+        Some("py") => Some(ts_python::language()),
+        _ => None,
+    }
 }
 
 fn get_outline(node: Node, source_code: &str) -> Vec<String> {
